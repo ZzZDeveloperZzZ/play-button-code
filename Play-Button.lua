5947410a79359465908d9ff1179e2421066b6b2b
ZzZDeveloperZzZ <101407155+ZzZDeveloperZzZ@users.noreply.github.com>
2022-03-11 17:59:24 +0400

    Add files via upload

---
+++ Play-Button.lua
@@ -0,0 +1,10 @@
+local script
+local Screen
+local screen =script.Parent.Screen
+local PlayButton = Screen.Play
+
+PlayButton.MouseButtonlDown:Connect(function()
+
+print("button was pressed or clicked")
+
+end)
